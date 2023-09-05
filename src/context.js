e4ba7640c50f672acde50f965acd6d13820deeeb
unknown <anands2807@gmail.com>
2023-09-05 21:22:31 +0530

    basic structuring done.

---
+++ src/context.js
@@ -0,0 +1,17 @@
+import React, {createContext, useContext} from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({children}) => {
+  return(
+    <AppContext.Provider>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+const useGlobalContext = () => {
+  return useContext(AppContext)
+}
+
+export {AppProvider, useGlobalContext}
